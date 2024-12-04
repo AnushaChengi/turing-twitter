33d215751b0eec5b699afc2e7bf10bdb8c4e8285
anushachengi <anusha.chengi@inttent.ai>
2024-12-04 10:00:47 +0000

    Initial commmit

---
+++ src/components /Home/Home.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+import '../Home/Home.css'
+
+function Home() {
+    return(
+        <div className='home-wrapper'>
+            
+        </div>
+    )
+
+}
+export default Home
